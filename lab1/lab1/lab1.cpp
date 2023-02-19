4abc9b528e8273adafd4511cc32639b8980da19f
BondarenkoOlena <bondarenko.olena@chnu.edu.ua>
2023-02-19 21:53:26 +0200

    new changes: slightly edited + fully working funks 3.1 + 3.2

--- lab1/lab1/lab1.cpp
+++ lab1/lab1/lab1.cpp
@@ -16,22 +16,188 @@
 #include <cstdlib>
 #include <time.h>
 #include <vector>
+#include <tuple>
 using namespace std;
 const int MAX_SIZE = 200;
+
+void task1(int N, int A[])
+{
+    //Із одновимірного масиву А розміру N побудувати масив В із всіх додатних елементів.
+    int B[MAX_SIZE], B_index = 0;
+    for (int i = 0; i < N; i++)
+    {
+        if (A[i] > 0)
+        {
+            B[B_index] = A[i];
+            B_index++;
+        }
+    }
+
+    if (B_index == 0)
+    {
+        printf("There is no positive numbers!");
+    }
+    else
+    {
+        for (int i = 0; i < B_index; i++)
+        {
+            printf("B[%d]: %d\n", i, B[i]);
+        }
+    }
+}
+
+void task2(int N, int A[])
+{
+    /*>>Зауваження. При розв’язуванні використати змінну(або декілька змінних) типу вказівник,
+яка би вказувала поточний елемент масиву(або на деякий елемент масиву), що
+розглядається (у приклад 5 змінна – вказівник pC вказує на поточний елемент масиву C).
+Заданий одномірний масив цілих чисел А розміру N.
+   Знайти номер останнього максимального елемента серед додатних елементів, починаючи з першого елемента, більшого заданого числа Т.*/
+    int T, max_index = 0, positive_index = 0;
+    printf("Enter T: ");
+    scanf_s("%d", &T);
+    int max = T;
+
+    for (int i = 0; i < N; i++)
+    {
+        if (A[i] > 0)
+        {
+            positive_index++;
+            if (A[i] > T && A[i] >= max)
+            {
+                max = A[i];
+                max_index = positive_index;
+            }
+        }
+    }
+
+    if (max_index == 0)
+    {
+        printf("There is no positive number bigger that %d!", T);
+    }
+    else
+    {
+        printf("Index of max positive number bigger than %d: %d", T, max_index);
+    }
+}
+
+void task3(int N, float X[])
+{
+    /*Задано дійсні величини a,b (a < b) і масив чисел X (n), n <= 200.
+Розробити програму, яка обчислює суму всіх X (i) < a , добуток всіх X (i) > b
+і знаходить max X(i) та min X(i) серед X(i)є[a,b], i=1,2,...,n.*/
+    float a, b;
+AB:  printf("Enter a: ");
+    scanf_s("%f", &a);
+    printf("Enter b: ");
+    scanf_s("%f", &b);
+    if (a >= b)
+    {
+        printf("a must be less than b!\n");
+        goto AB;
+    }
+
+    float Sum = 0, Mult = 1, max = a, min = b;
+    bool Sum_flag = false, Mult_flag = false, MinMax_flag = false;
+    for (int i = 0; i < N; i++)
+    {
+        if (X[i] < a)
+        {
+            Sum += X[i];
+            Sum_flag = true;
+        }
+        else if (X[i] > b)
+        {
+            Mult *= X[i];
+            Mult_flag = true;
+        }
+        else
+        {
+            MinMax_flag = true;
+            if (X[i] > max)
+            {
+                max = X[i];
+            }
+            if (X[i] < min)
+            {
+                min = X[i];
+            }
+        }
+    }
+
+    if (Sum_flag == true)
+    {
+        printf("Sum of numbers less than a = %0.2f\n", Sum);
+    }
+    else
+    {
+        printf("There is no number less than a\n");
+    }
+    if (Mult_flag == true)
+    {
+        printf("Mult of numbers bigger than b = %0.2f\n", Mult);
+    }
+    else
+    {
+        printf("There is no number bigger than b");
+    }
+    if (MinMax_flag == true)
+    {
+        printf("Max number between a and b = %0.2f\n", max);
+        printf("Min number between a and b = %0.2f\n", min);
+    }
+    else
+    {
+        printf("There is no number between a and b");
+    }
+}
 
 int realise_functions()
 {
     int function_number = 0;
-    printf("Possible functions:\n   Function 1: Input/Output from console to file\n   Function 2: Input array from console\n   Function 3: Input random numbers to array\n   Function 4: Output array to binary file\n");
+    printf("Possible functions:\n   Function 1: Input/Output from console to file\n   Function 2: Input array from console + Input random numbers to array + Output array to binary file\n   Function 3: Input/Output array from file to file to console\n   Function 4: InputOutput array from file to vector to console\n");
     printf("Enter function number you want to use: ");
     scanf_s("%d", &function_number);
     system("cls");
     return function_number;
 }
+int realise_tasks()
+{
+    printf("Tasks:\n   Task 1: Array of positive numbers\n   Task 2: Index of last max number\n   Task 3: Find stuff in between\n");
+    int task_manager = 0;
+    printf("Enter task number you want to do: ");
+    scanf_s("%d", &task_manager);
+    system("cls");
+    return task_manager;
+}
 
 //Функції введення масивів з консолі та запис масивів в текстовий файл(текстові файли);
-void output_file_for_int(int N, int A[])
+void output_file_for_int(int task_manager)
 {
+RESIZE:  int N;
+    printf("Enter N: ");
+    scanf_s("%d", &N);
+    if (N <= 0)
+    {
+        printf("Please enter N > 0!\n");
+        goto RESIZE;
+
+    }
+    int A[MAX_SIZE];
+    for (int i = 0; i < N; i++)
+    {
+        printf("Enter A[%d]: ", i);
+        scanf_s("%d", &A[i]);
+    }
+    if (task_manager == 1)
+    {
+        task1(N, A);
+    }
+    else
+    {
+        task2(N, A);
+    }
+
     ofstream MyFile("file.txt");
 
     for (int i = 0; i < N; i++)
@@ -41,13 +207,28 @@ void output_file_for_int(int N, int A[])
 
     MyFile.close();
 }
-void output_file_for_float(int N, float A[])
+void output_file_for_float()
 {
+RESIZE:  int N;
+    printf("Enter N: ");
+    scanf_s("%d", &N);
+    if (N <= 0)
+    {
+        printf("Please enter N > 0!\n");
+        goto RESIZE;
+    }
+    float X[MAX_SIZE];
+    for (int i = 0; i < N; i++)
+    {
+        printf("Enter X[%d]: ", i);
+        scanf_s("%f", &X[i]);
+    }
+    task3(N, X);
     ofstream MyFile("file.txt");
 
     for (int i = 0; i < N; i++)
     {
-        MyFile << A[i] << "   ";
+        MyFile << X[i] << "   ";
     }
 
     MyFile.close();
@@ -69,6 +250,7 @@ int* random_array_for_int(int N)
     for (int i = 0; i < N; i++)
     {
         A[i] = rand() % 100;
+        printf("A[%d]: %d\n", i, A[i]);
     }
     return A;
 }
@@ -79,11 +261,22 @@ float* random_array_for_float(int N)
     for (int i = 0; i < N; i++)
     {
         X[i] = rand() % 100;
+        printf("A[%d]: %f\n", i, X[i]);
     }
     return X;
 }
-void output_binary_file_for_int(int N, int A[])
+void output_binary_file_for_int(int task_manager)
 {
+    int N = input_arrays_size();
+    int* A = random_array_for_int(N);
+    if (task_manager == 1)
+    {
+        task1(N, A);
+    }
+    else
+    {
+        task2(N, A);
+    }
     ofstream MyFile("binary.dat", ios::out | ios::binary);
     for (int i = 0; i < N; i++)
     {
@@ -91,12 +284,15 @@ void output_binary_file_for_int(int N, int A[])
     }
     MyFile.close();
 }
-void output_binary_file_for_float(int N, float A[])
+void output_binary_file_for_float()
 {
+    int N = input_arrays_size();
+    float* X = random_array_for_float(N);
+    task3(N, X);
     ofstream MyFile("binary.dat", ios::out | ios::binary);
     for (int i = 0; i < N; i++)
     {
-        MyFile.write((char*)&A[i], sizeof(float));
+        MyFile.write((char*)&X[i], sizeof(float));
     }
     MyFile.close();
 }
@@ -110,7 +306,7 @@ void read_from_file_to_file_to_console()
 }
 
 //Функцію читає дані з файлів в контейнер (vector, valarray або array) та з контейнера дані виводиться в консоль.
-vector<int> read_from_file_to_container_to_console_for_int()
+void read_from_file_to_container_to_console_for_int(int task_manager)
 {
     std::streampos file_size;
     std::ifstream file("file.txt", std::ios::out);
@@ -124,11 +320,20 @@ vector<int> read_from_file_to_container_to_console_for_int()
 
     for (int i = 0; i < A.size(); i++)
     {
-        printf("A[%d]: %d", i, A[i]);
+        printf("A[%d]: %d\n", i, A[i]);
     }
-    return A;
+
+    int* B = &A[0];
+    if (task_manager == 1)
+    {
+        task1(A.size(), B);
+    }
+    else
+    {
+        task2(A.size(), B);
+    }
 }
-vector<float> read_from_file_to_container_to_console_for_float()
+void read_from_file_to_container_to_console_for_float()
 {
     std::streampos file_size;
     std::ifstream file("file.txt", std::ios::out);
@@ -142,200 +347,66 @@ vector<float> read_from_file_to_container_to_console_for_float()
 
     for (int i = 0; i < X.size(); i++)
     {
-        printf("X[%d]: %f", i, X[i]);
-    }
-    return X;
-}
-
-void task1()
-{
-    int function_number = realise_functions();
-    //Із одновимірного масиву А розміру N побудувати масив В із всіх додатних елементів.
-    int A[MAX_SIZE], N, B[MAX_SIZE], B_index = 0;
-RESIZE1:  N = input_arrays_size();
-    if (N > 0)
-    {
-        for (int i = 0; i < N; i++)
-        {
-            printf("Enter A[%d]: ", i);
-            scanf_s("%d", &A[i]);
-            if (A[i] > 0)
-            {
-                B[B_index] = A[i];
-                B_index++;
-            }
-        }
-
-        if (B_index == 0)
-        {
-            printf("There is no positive numbers!");
-        }
-        else
-        {
-            for (int i = 0; i < B_index; i++)
-            {
-                printf("B[%d]: %d\n", i, B[i]);
-            }
-        }
-    }
-    else
-    {
-        printf("Please enter N > 0!\n");
-        goto RESIZE1;
-    }
-}
-
-void task2()
-{
-    int function_number = realise_functions();
-    /*>>Зауваження. При розв’язуванні використати змінну(або декілька змінних) типу вказівник,
-яка би вказувала поточний елемент масиву(або на деякий елемент масиву), що
-розглядається (у приклад 5 змінна – вказівник pC вказує на поточний елемент масиву C).
-Заданий одномірний масив цілих чисел А розміру N.
-   Знайти номер останнього максимального елемента серед додатних елементів, починаючи з першого елемента, більшого заданого числа Т.*/
-    int A[MAX_SIZE], N, T, max_index = 0, positive_index = 0;
-    printf("Enter T: ");
-    scanf_s("%d", &T);
-RESIZE2:  N = input_arrays_size();
-    if (N > 0)
-    {
-        for (int i = 0; i < N; i++)
-        {
-            printf("Enter A[%d]: ", i);
-            scanf_s("%d", &A[i]);
-            if (A[i] > 0)
-            {
-                positive_index++;
-                if (A[i] > T)
-                {
-                    max_index = positive_index;
-                }
-            }
-        }
-
-        if (max_index == 0)
-        {
-            printf("There is no positive number bigger that %d!", T);
-        }
-        else
-        {
-            printf("Index of max positive number bigger than %d: %d", T, max_index);
-        }
-    }
-    else
-    {
-        printf("Please enter N > 0!\n");
-        goto RESIZE2;
-    }
-}
-
-void task3()
-{
-    int function_number = realise_functions();
-    /*Задано дійсні величини a,b (a < b) і масив чисел X (n), n <= 200. 
-Розробити програму, яка обчислює суму всіх X (i) < a , добуток всіх X (i) > b
-і знаходить max X(i) та min X(i) серед X(i)є[a,b], i=1,2,...,n.*/
-    float a, b, X[MAX_SIZE];
-AB:  printf("Enter a: ");
-    scanf_s("%f", &a);
-    printf("Enter b: ");
-    scanf_s("%f", &b);
-    if (a >= b)
-    {
-        printf("a must be less than b!\n");
-        goto AB;
+        printf("X[%d]: %f\n]", i, X[i]);
     }
 
-    int N;
-RESIZE3:  N = input_arrays_size();
-    if (N > 0)
-    {
-        float Sum = 0, Mult = 1, max = a, min = b;
-        bool Sum_flag = false, Mult_flag = false, MinMax_flag = false;
-        for (int i = 0; i < N; i++)
-        {
-            printf("Enter X[%d]: ", i);
-            scanf_s("%f", &X[i]);
-            if (X[i] < a)
-            {
-                Sum += X[i];
-                Sum_flag = true;
-            }
-            else if (X[i] > b)
-            {
-                Mult *= X[i];
-                Mult_flag = true;
-            }
-            else
-            {
-                MinMax_flag = true;
-                if (X[i] > max)
-                {
-                    max = X[i];
-                }
-                if (X[i] < min)
-                {
-                    min = X[i];
-                }
-            }
-        }
-        
-        if (Sum_flag == true)
-        {
-            printf("Sum of numbers less than a = %0.2f\n", Sum);
-        }
-        else
-        {
-            printf("There is no number less than a");
-        }
-        if (Mult_flag == true)
-        {
-            printf("Mult of numbers bigger than b = %0.2f\n", Mult);
-        }
-        else
-        {
-            printf("There is no number bigger than b");
-        } 
-        if (MinMax_flag == true)
-        {
-            printf("Max number between a and b = %0.2f\n", max);
-            printf("Min number between a and b = %0.2f\n", min);
-        }
-        else
-        {
-            printf("There is no number between a and b");
-        }
-    }
-    else
-    {
-        goto RESIZE3;
-    }
+    float* Y = &X[0];
+    task3(X.size(), Y);
 }
 
 int main()
 {
-TASK:  printf("Tasks:\n   Task 1: Array of positive numbers\n   Task 2: Index of last max number\n   Task 3: Find stuff in between\n");
-    int task_manager = 0;
-    printf("Enter task number you want to do: ");
-    scanf_s("%d", &task_manager);
-    system("cls");
-
-    if (task_manager == 1)
+TASK:  int task_manager = realise_tasks();
+    if (task_manager > 3 || task_manager < 1)
     {
-        task1();
+        printf("Please enter number from 1 to 3!\n");
+        goto TASK;
     }
-    else if (task_manager == 2)
+FUNK:  int function_manager = realise_functions();
+    if (function_manager > 4 || function_manager < 1)
     {
-        task2();
+        printf("Please enter number from 1 to 4!\n");
+        goto FUNK;
     }
-    else if (task_manager == 3)
+    if (task_manager == 3)
     {
-        task3();
+        switch (function_manager)
+        {
+        case 1:
+            output_file_for_float();
+            break;
+        case 2:
+            output_binary_file_for_float();
+            break;
+        case 3:
+            read_from_file_to_file_to_console();
+            break;
+        case 4:
+            read_from_file_to_container_to_console_for_float();
+            break;
+        default:
+            break;
+        }
     }
     else
     {
-        printf("Please enter number from 1 to 3!");
-        goto TASK;
+        switch (function_manager)
+        {
+        case 1:
+            output_file_for_int(task_manager);
+            break;
+        case 2:
+            output_binary_file_for_int(task_manager);
+            break;
+        case 3:
+            read_from_file_to_file_to_console();
+            break;
+        case 4:
+            read_from_file_to_container_to_console_for_int(task_manager);
+            break;
+        default:
+            break;
+        }
     }
 
     string answer;
@@ -358,14 +429,3 @@ FIN:  printf("\nDo you want to do one more task? Enter 'yes' if you do, and 'no'
         goto FIN;
     }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
